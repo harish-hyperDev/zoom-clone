f86b04a99a438819faa730661d0909c17c293fec
harish-hyperDev <coolprogrammer.777@gmail.com>
2022-12-06 00:11:37 +0530

    initial commit

---
+++ public/script.js
@@ -0,0 +1,8 @@
+var io = require('socket.io')(http);
+const socket = io('/')
+
+socket.emit('join-room', ROOM_ID, 10)
+
+socket.on ('user-connected', userId => {
+    console.log("user connected: ", userId)
+})
